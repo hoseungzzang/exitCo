4260368b3539a1c85cbf7fec05dc40a6ccb4d41c
hoseungzzang <wnghtmd99@naver.com>
2022-07-04 23:34:29 +0900

    json파싱해오기 아직 실패

--- exitCo/src/main/java/com/hs/exitCo/controller/api/MainApiController.java
+++ exitCo/src/main/java/com/hs/exitCo/controller/api/MainApiController.java
@@ -1,16 +1,28 @@
 package com.hs.exitCo.controller.api;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
@@ -25,13 +37,14 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hs.exitCo.dto.KakaoDto;
+import com.hs.exitCo.model.GetSido;
 import com.hs.exitCo.model.KakaoGetDong;
 
 @Controller
 public class MainApiController {
-
+	final String encKey = "QlYtJR%2BvwVIrtLun4CUyEjfxjV8mrqQBpudJ%2BDSKB7bGIdYn7rnj%2FfLm3bYh%2FwvCDCowCpFZmdblGbXmdX99%2BA%3D%3D";
 	@PostMapping("/mainApi/sidoSelect")
-	public String callDong(@RequestBody KakaoDto kakao) {
+	public String callDong(@RequestBody KakaoDto kakao) throws IOException, URISyntaxException {
 
 
 	        RestTemplate restTemplate = new RestTemplate();
@@ -48,7 +61,6 @@ public class MainApiController {
 					String.class
 					);
 
-			
 			ObjectMapper objectMapper = new ObjectMapper();
 			 KakaoGetDong kakaoGetDong =null;
 			try {
@@ -64,7 +76,32 @@ public class MainApiController {
 				e.printStackTrace();
 			}
 
+			RestTemplate restTemplate2 = new RestTemplate();
+			HttpHeaders headers2 = new HttpHeaders();
+			 HttpEntity<String> sidoEntity = new HttpEntity<>(headers2);
+			 
+			 StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List"); /*URL*/
+		        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="+ encKey); /*Service Key*/
+		        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
+		        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
+		        urlBuilder.append("&" + URLEncoder.encode("type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*호출문서 형식*/
+		        URI url = new URI(urlBuilder.toString());
+		        ResponseEntity<String> response2 = restTemplate2.exchange(url, HttpMethod.GET, sidoEntity, String.class);
 
+		    	ObjectMapper objectMapper2 = new ObjectMapper();
+				 GetSido getSido =null;
+				try {
+					System.out.println(response2.getBody());
+					getSido = objectMapper2.readValue(response2.getBody(), GetSido.class);
+					System.out.println(getSido.getRow());
+					
+				} catch (JsonMappingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		return "";
 	}
 }
