00ddc3f5b1e57daef6c5e8a43acc2c9c53b18923
hoseungzzang <wnghtmd99@naver.com>
2022-07-07 19:22:55 +0900

    마커 뿌리기 완료

--- exitCo/src/main/resources/static/js/kakao.js
+++ exitCo/src/main/resources/static/js/kakao.js
@@ -1,4 +1,6 @@
-
+var positions = [];
+var marker = [];
+var overlay = [];
 window.onload = function() {
 	window.navigator.geolocation.getCurrentPosition(current_position);
 
@@ -23,24 +25,21 @@ function current_position(position) {
 		contentType: "application/json; charset=utf-8", //데이터 타입 명시
 		dataType: "json", // 응답이 왔을때의 데이터타입을 자바스크립트 오브젝트로 변환해준다.
 		success: function(data) {
-				var map = new kakao.maps.Map(container, options);
-				var positions=[];
-				
+			window.positions = [];
 			for (var i = 0; i < data.length; i++) {
-				var obj={};
-			
-			obj.title = data[i][0];
-			obj.latlng = new kakao.maps.LatLng( data[i][1],  data[i][2]);
-			
-				positions[i] = obj;
-			}
-			console.log(positions.length);
+				var obj = {};
 
-			
+				obj.title = data[i][0];
+				obj.latlng = new kakao.maps.LatLng(data[i][1], data[i][2]);
+
+				window.positions[i] = obj;
+			}
+
+			var map = new kakao.maps.Map(container, options);
 			var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
-			
-for (var i = 0; i < data.length; i++) {
+
+			for (var i = 0; i < data.length; i++) {
 				// 마커 이미지의 이미지 크기 입니다
 				var imageSize = new kakao.maps.Size(24, 35);
 
@@ -48,14 +47,31 @@ for (var i = 0; i < data.length; i++) {
 				var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
 				// 마커를 생성합니다
-				var marker = new kakao.maps.Marker({
-					map: map, // 마커를 표시할 지도
-					position: positions[i].latlng,// 마커를 표시할 위치
-					title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-					image: markerImage // 마커 이미지 
+				window.marker[i] = new kakao.maps.Marker({
+					position: window.positions[i].latlng,// 마커를 표시할 위치
 				});
+				marker[i].setMap(map);
+
+				
 			}
+
+
+
 		}
 	})
 
 }
+
+
+/*
+var iwContent = '<div style="padding:8px;">'+positions[i].title
+				+' <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">커뮤니티 조회</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+					iwPosition = positions[i].latlng; //인포윈도우 표시 위치입니다
+				// 인포윈도우를 생성합니다
+				var infowindow = new kakao.maps.InfoWindow({
+					position: iwPosition,
+					content: iwContent
+				});
+				// 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+				infowindow.open(map, marker);*/
+
--- exitCo/src/main/webapp/WEB-INF/views/main.jsp
+++ exitCo/src/main/webapp/WEB-INF/views/main.jsp
@@ -5,6 +5,15 @@
 <head>
 <meta charset="UTF-8">
 <title>전국 대피소 찾기</title>
+<script charset="UTF-8">
+	new daum.roughmap.Lander({
+		"timestamp" : "1613788870358",
+		"key" : "24iif",
+		"mapWidth" : "640",
+		"mapHeight" : "360"
+	}).render();
+</script>
+<script charset="UTF-8" class="daum_roughmap_loader_script" src="https://ssl.daumcdn.net/dmaps/map_js_init/roughmapLoader.js"></script>
 
 </head>
 <style>
@@ -12,6 +21,8 @@ html,body{
 width:100%;  height:100%;
 overflow: visible;
 }
+
+
 </style>
 <body>
 <%@ include file="./layout/header.jsp"%>
