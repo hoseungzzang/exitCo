7ceed1b18e3f1267bc2ef40d01f091dc542f97d6
hoseungzzang <wnghtmd99@naver.com>
2022-07-07 23:45:51 +0900

    마커 안에 내용담고 이벤트 입히기 성공

--- exitCo/src/main/resources/static/js/kakao.js
+++ exitCo/src/main/resources/static/js/kakao.js
@@ -1,10 +1,12 @@
-var positions = [];
-var marker = [];
+
+
 var overlay = [];
+
 window.onload = function() {
 	window.navigator.geolocation.getCurrentPosition(current_position);
 
 };
+
 function current_position(position) {
 	var container = document.getElementById('map');
 	var options = {
@@ -25,14 +27,15 @@ function current_position(position) {
 		contentType: "application/json; charset=utf-8", //데이터 타입 명시
 		dataType: "json", // 응답이 왔을때의 데이터타입을 자바스크립트 오브젝트로 변환해준다.
 		success: function(data) {
-			window.positions = [];
+
+			var positions = [];
 			for (var i = 0; i < data.length; i++) {
 				var obj = {};
 
 				obj.title = data[i][0];
 				obj.latlng = new kakao.maps.LatLng(data[i][1], data[i][2]);
 
-				window.positions[i] = obj;
+				positions[i] = obj;
 			}
 
 			var map = new kakao.maps.Map(container, options);
@@ -47,18 +50,37 @@ function current_position(position) {
 				var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
 				// 마커를 생성합니다
-				window.marker[i] = new kakao.maps.Marker({
-					position: window.positions[i].latlng,// 마커를 표시할 위치
-				});
-				marker[i].setMap(map);
+				var marker = new kakao.maps.Marker({
+					position: positions[i].latlng,// 마커를 표시할 위치
+					clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
 
-				
+				});
+				marker.setMap(map);
+				// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+				var iwContent = '<div style="padding:5px;">' + positions[i].title + '</div><br><a href="">커뮤니티 이동</a>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+					iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+				// 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+				// 이벤트 리스너로는 클로저를 만들어 등록합니다 
+				// for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+				var infowindow = new kakao.maps.InfoWindow({
+					content: iwContent,
+					removable: iwRemoveable
+				});
+				kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow));
+			}
+
+			// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+			function makeOverListener(map, marker, infowindow) {
+				return function() {
+					infowindow.open(map, marker);
+				};
 			}
 
 
-
 		}
+
 	})
+
 
 }
 
